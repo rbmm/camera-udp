495ec1067ae2d4a80078e8f8529b0bbc4ebb8bdb
rbmm <_dm_sh_@tutanota.com>
2026-03-28 02:42:44 +0200

    *

--- camera server/ep.cpp
+++ camera server/ep.cpp
@@ -8,9 +8,156 @@ _NT_BEGIN
 
 BOOL StartServer(ULONG64 crc2, ULONG64 crc1);
 
+union TIDFA {
+	LONG m_dwThreadId;
+	struct {
+		ULONG m_wait : 1;
+		ULONG m_alert : 1;
+	};
+
+	TIDFA(ULONG dwThreadId = GetCurrentThreadId()) : m_dwThreadId(dwThreadId)
+	{
+	}
+
+	void Alert();
+
+	NTSTATUS WaitForAlert(_In_opt_ PLARGE_INTEGER Timeout = 0);
+};
+
+void TIDFA::Alert()
+{
+	union {
+		ULONG dwThreadId;
+		struct {
+			ULONG wait : 1;
+			ULONG alert : 1;
+		};
+	};
+	union {
+		ULONG _dwThreadId;
+		struct {
+			ULONG _wait : 1;
+			ULONG _alert : 1;
+		};
+	};
+
+	dwThreadId = m_dwThreadId;
+
+	for (;;)
+	{
+		_dwThreadId = dwThreadId;
+
+		alert = 1, wait = 0;
+
+		if (_dwThreadId == (dwThreadId = InterlockedCompareExchange(&m_dwThreadId, dwThreadId, _dwThreadId)))//--0
+		{
+			if (wait && !alert)
+			{
+				wait = 0;
+				if (0 > ZwAlertThreadByThreadId((HANDLE)(ULONG_PTR)dwThreadId))
+				{
+					__debugbreak();
+				}
+			}
+
+			break;
+		}
+	}
+}
+
+NTSTATUS TIDFA::WaitForAlert(_In_opt_ PLARGE_INTEGER Timeout /*= 0*/)
+{
+	union {
+		ULONG dwThreadId;
+		struct {
+			ULONG wait : 1;
+			ULONG alert : 1;
+		};
+	};
+	union {
+		ULONG _dwThreadId;
+		struct {
+			ULONG _wait : 1;
+			ULONG _alert : 1;
+		};
+	};
+
+	dwThreadId = m_dwThreadId;
+
+	for (;;)
+	{
+		_dwThreadId = dwThreadId;
+
+		if (wait)
+		{
+			__debugbreak();
+		}
+
+		if (alert)
+		{
+			alert = 0;
+			wait = 0;
+		}
+		else
+		{
+			wait = 1;
+		}
+
+		BOOL bNeedWait = wait;
+
+		if (_dwThreadId == (dwThreadId = InterlockedCompareExchange(&m_dwThreadId, dwThreadId, _dwThreadId)))//--1
+		{
+			if (bNeedWait)
+			{
+				NTSTATUS status = ZwWaitForAlertByThreadId(this, Timeout);
+
+				dwThreadId = m_dwThreadId;
+
+				for (;;)
+				{
+					_dwThreadId = dwThreadId;
+
+					wait = 0, alert = 0;
+
+					if (_dwThreadId == (dwThreadId = InterlockedCompareExchange(&m_dwThreadId, dwThreadId, _dwThreadId)))//--2
+					{
+						if (alert)
+						{
+							if (STATUS_ALERTED != status)
+							{
+								LARGE_INTEGER zt = {};
+
+								ULONG spinCount = 0;
+								while (STATUS_ALERTED != ZwWaitForAlertByThreadId(this, &zt))
+								{
+									switch (++spinCount >> 3)
+									{
+									case 0: YieldProcessor();
+										continue;
+									case 1: SwitchToThread();
+										continue;
+									}
+
+									Sleep(1);
+								}
+
+								return STATUS_ALERTED;
+							}
+						}
+
+						return status;
+					}
+				}
+			}
+
+			return STATUS_ALERTED;
+		}
+	}
+}
+
 class CSvc : public CSvcBase
 {
-	HANDLE _hThreadId = (HANDLE)GetCurrentThreadId();
+	TIDFA m_tid;
 
 	virtual HRESULT Run()
 	{
@@ -45,7 +192,7 @@ class CSvc : public CSvcBase
 									if (StartServer(crc2, crc1))
 									{
 										DbgPrint("Start Server\r\n");
-										ZwWaitForAlertByThreadId(0, const_cast<PLARGE_INTEGER>(&Interval));
+										m_tid.WaitForAlert();
 										DbgPrint("alert\r\n");
 									}
 									else
@@ -61,7 +208,7 @@ class CSvc : public CSvcBase
 						}
 						else
 						{
-							ZwWaitForAlertByThreadId(0, const_cast<PLARGE_INTEGER>(&Interval));
+							m_tid.WaitForAlert();
 						}
 
 					} while (SERVICE_STOPPED != m_dwTargetState);
@@ -85,7 +232,7 @@ class CSvc : public CSvcBase
 		case SERVICE_CONTROL_CONTINUE:
 		case SERVICE_CONTROL_PAUSE:
 		case SERVICE_CONTROL_STOP:
-			return RtlNtStatusToDosErrorNoTeb(ZwAlertThreadByThreadId(_hThreadId));
+			return m_tid.Alert(), NOERROR;
 		}
 
 		return ERROR_SERVICE_CANNOT_ACCEPT_CTRL;
